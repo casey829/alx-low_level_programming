2b4d7a23c3b91854b2539ccf57800d4bad3c66e4
casey829 <caseymwangi31@gmail.com>
2022-07-01 10:24:42 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print single numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <= 10; num++)
+		printf('%d', num);
+	putchar('\n');
+
+	return (0);
+}
