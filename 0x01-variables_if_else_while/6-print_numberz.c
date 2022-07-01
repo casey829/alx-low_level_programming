dd8fddbf2401d3b6353d4f1e0d7b4a0a501995a0
casey829 <caseymwangi31@gmail.com>
2022-07-01 11:06:54 -0700

    number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <strio.h>
+
+/**
+ * main - Print all single digit numbers using only putchar
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+
+	putchar("\n");
+}
