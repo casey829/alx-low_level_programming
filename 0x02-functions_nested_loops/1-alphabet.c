e52ce3c38ecd6528eeae2c4176c72679c631da9f
casey829 <caseymwangi31@gmail.com>
2022-07-05 11:45:04 -0700

    add loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the alphabet
+ *
+ * Return:Always  0 (Success)
+ */
+int num(void)
+{
+	int i = 'a';
+
+	for(num = i; i <= 'z'; i++)
+	{
+		_putchar('%d', num);
+		_putchar('\n');
+
+		return (0);
+	}
+}
