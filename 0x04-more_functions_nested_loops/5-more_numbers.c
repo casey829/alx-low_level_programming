a908d43c74cb9063eca5e72d0c16df2e222c9387
casey829 <caseymwangi31@gmail.com>
2022-07-07 13:45:15 -0700

    add5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,18 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, tm;
 
-	for (i = 0; i <= 14; i++)
+	for (tm = 0; tm < 10; tm++)
 	{
-		_putchar(i);
-		_putchar('%d', 10);
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
