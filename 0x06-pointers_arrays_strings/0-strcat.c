5b91da2ac96e4280e1ba73828f584ac072a6aa15
casey829 <caseymwangi31@gmail.com>
2022-07-14 10:03:00 -0700

    001

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates the string
+ * @dest: A pointer to concanetaed string
+ * @src: Sources string
+ * Return: A pointer to destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int ind = 0, dest_leng = 0;
+
+	while (dest[ind++])
+		dest_leng++;
+
+	for (ind = 0; src[ind]; ind++)
+		dest[dest_leng++] = src[ind];
+
+	return (dest);
+}
