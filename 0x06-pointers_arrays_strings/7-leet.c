40a8538b96cf1da151890b4a8e6850142a6043c6
casey829 <caseymwangi31@gmail.com>
2022-07-14 14:16:56 -0700

    function that encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,7 +11,7 @@ char *leet(char *s)
 	int indx1 = 0, indx2;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[indx1])
+	while (s[indx1])
 	{
 		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
