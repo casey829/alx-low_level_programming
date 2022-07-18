beb4e8a2b82563c5db9f36d2cad2188d91362084
casey829 <caseymwangi31@gmail.com>
2022-07-18 13:38:29 -0700

    that sets a value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of pointer
+ * @s: source address
+ * @to: target adress
+ * Return: no return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
