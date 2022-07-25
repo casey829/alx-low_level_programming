a794d5080a4343bd3a4a52696bc855d2d745fb86
casey829 <caseymwangi31@gmail.com>
2022-07-25 07:30:33 -0700

    ptint number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of parameters
+ * @argv: array of parameters
+ *
+ * Return: 0 always
+ */
+int main(int argc, __noerr char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
