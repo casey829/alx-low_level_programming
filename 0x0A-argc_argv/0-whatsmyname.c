82716c53a2bb95ef9ca9721edbcc903c2d59e491
casey829 <caseymwangi31@gmail.com>
2022-07-25 07:26:19 -0700

    print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: an array of a command is listed
  * Return: 0 for success
  */
-int main(int argc, char *argv[])
+int main(__noerr int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
