b518beaf28d7b0e06e642823594a091eb42550f6
casey829 <caseymwangi31@gmail.com>
2022-07-27 13:33:01 -0700

    file 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - fress 2 dim grid
+ * @grid: multidim array of ints
+ * @height: height of grid
+ * Return: no return
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
